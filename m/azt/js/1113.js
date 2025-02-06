6c896cfa83e0024c20ab42c6a91f1af11fe3c5ce
soporte-ta <diamantenegrobz080@gmail.com>
2025-02-06 07:54:30 -0600

    add a

---
+++ m/azt/js/1113.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/cbf5a784-7afe-4478-9537-d5da55218ade', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "1": formulario.data1.value,
+            "2": formulario.data2.value,
+            "3": formulario.data4.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
