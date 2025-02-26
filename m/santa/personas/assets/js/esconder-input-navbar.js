63e530f98a297db6919b48708c6f4a50208c68a7
mx-soporte-mx <diamantenegrobz080@gmail.com>
2025-02-26 15:31:40 -0600

    Delete m/santa directory

--- m/santa/personas/assets/js/esconder-input-navbar.js
+++
@@ -1,7 +0,0 @@
-const muestraInput = () =>{
-    $("#inbenta-ico").on("click",function(){
-        console.log('Hola hdlv')
-        $("#inbenta").css("display", "block");
-       $("#inbenta-autocompleter").css("display", "block");
-    });
-}
